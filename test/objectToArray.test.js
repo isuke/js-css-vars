b74a12dbefceb7f1e2a9e784782579e85a5cc17d
isuke <isuke770@gmail.com>
2020-01-22 21:03:29 +0900

    up: upgrade eslint and prettier

--- test/objectToArray.test.js
+++ test/objectToArray.test.js
@@ -4,6 +4,10 @@ const objectToArray = require('../lib/objectToArray')
 
 test('.objectToArray', (t) => {
   const object = { aaa: { bbb: { ccc: 123, eee: 234 } }, fff: 345 }
-  const expected = [['aaa', 'bbb', 'ccc', 123], ['aaa', 'bbb', 'eee', 234], ['fff', 345]]
+  const expected = [
+    ['aaa', 'bbb', 'ccc', 123],
+    ['aaa', 'bbb', 'eee', 234],
+    ['fff', 345],
+  ]
   t.deepEqual(objectToArray(object), expected)
 })
